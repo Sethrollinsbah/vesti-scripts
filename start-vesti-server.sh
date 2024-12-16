e8edd98c138e5b9a6e26cb88583ddd17312ae791
Seth Rollins <sethrollins@Seths-MacBook-Air.local>
2024-12-16 11:29:41 -0500

    jq output

--- start-vesti-server.sh
+++ start-vesti-server.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-aws ec2 start-instances --instance-ids "i-08393424829b3ecc7"
+# Start the EC2 instance
+aws ec2 start-instances --instance-ids "i-08393424829b3ecc7" | jq
+
+# Navigate to the home directory
+cd ~
+
+# Fetch the public DNS name of the instance and store it in a variable
+PUBLIC_DNS_NAME=$(aws ec2 describe-instances --instance-ids "i-08393424829b3ecc7" | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicDnsName')
+
+# Connect to the instance via SSH
+ssh -i "elk.pem" ec2-user@$PUBLIC_DNS_NAME
+
--- stop-vesti-server.sh
+++ stop-vesti-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws ec2 stop-instances --instance-ids "i-08393424829b3ecc7"
+aws ec2 stop-instances --instance-ids "i-08393424829b3ecc7" | jq
+
+
